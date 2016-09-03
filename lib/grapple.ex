225c0102bc7f976ac182d19fd85fdbc91dfc67b6
camirmas <cjirmas@gmail.com>
2016-09-03 13:40:18 -0700

    Add Phoenix.PubSub to supervision tree :evergreen_tree:

--- lib/grapple.ex
+++ lib/grapple.ex
@@ -2,6 +2,13 @@ defmodule Grapple do
   use Application
 
   def start(_type, _args) do
-    Grapple.Supervisor.start_link([])
+    import Supervisor.Spec
+
+    children = [
+      supervisor(Phoenix.PubSub.PG2, [Grapple.PubSub, []])
+    ]
+
+    opts = [strategy: :one_for_one, name: Grapple.Supervisor]
+    Supervisor.start_link(children, opts)
   end
 end
